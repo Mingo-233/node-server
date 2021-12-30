9a4f1ae19d1c74c1d32a375594431d90d22659d6
mingo <mingo233@qq.com>
2021-12-30 12:36:02 +0800

    feat:cache

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express')
 const fs = require('fs')
 const app = new express()
 const port = 3000
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', '*')
+    // res.setHeader('Content-Type', 'application/json;charset=utf-8')
+    // res.setHeader("Access-Control-Allow-Headers", "Content-Type,Key");
+    // res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
 
+    if (!res.getHeader('Cache-Control') || !res.getHeader('Expires')) {
+        res.setHeader("Cache-Control", "public, max-age=345600"); // ex. 4 days in seconds. 
+        res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString()); // in ms. 
+        console.log(res);
+    }
+    next()
+})
 const sleepFun = time => {
     return new Promise(res => {
         setTimeout(() => {
@@ -25,7 +37,8 @@ app.use(filter)
 app.use('/static/', express.static('./static/'))
 
 app.get('/', function (req, res, next) {
-    fs.readFile('./index2.html', 'UTF-8', (err, data) => {
+
+    fs.readFile('./index.html', 'UTF-8', (err, data) => {
         if (err) return
         res.send(data)
     })
