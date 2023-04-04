2b4b362741efced1dda9304a99093edd3f7589e3
mingo <mingo233@qq.com>
2023-04-04 20:54:45 +0800

    update

--- static/index.js
+++
@@ -1,7 +0,0 @@
-// static/index.js
-var p = document.querySelector('p');
-console.log('index.js'+p);
-console.log(p);
-var style = window.getComputedStyle(p, null);
-console.log(style.color);
-console.log(new Date());
