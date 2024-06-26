852f5ff41a3a7ed48eb925d016dfecbf90f5574e
Mingo <69039945+Mingo-233@users.noreply.github.com>
2024-06-26 17:02:18 +0800

    feat: socket

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
-const express = require('express')
-const fs = require('fs')
-const app = new express()
-const port = 3123
 
-
-
-
-app.use('/static/', express.static('./static/', {
-    setHeaders: function (res, path, stat) {
-        console.log(res);
-        res.set('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-        res.header('Pragma', 'no-cache');
+const sirv = require('sirv');
+const serve = sirv('client', {
+    dev: true, // 开发模式下启用更详细的日志
+});
+let askTaskResolve = null
+const server = require('http').createServer(async (req, res) => {
+    // 自定义路由
+    if (req.url === '/getToken') {
+        const token = await getClientToken()
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            code: 200,
+            data: token
+        }));
+    } else {
+        // 处理静态资源请求
+        serve(req, res);
     }
-}))
+});
 
-// app.get('/', function (req, res, next) {
-//     fs.readFile('./index2.html', 'UTF-8', (err, data) => {
-//         if (err) return
-//         res.send(data)
-//     })
-// })
-// 禁用缓存
+const io = require('socket.io')(server);
+io.on('connection', client => {
+    console.log('connection', client.id);
+    client.on('receiveToken', token => {
+        console.log('receiveToken', token);
+        askTaskResolve && askTaskResolve(token)
+        askTaskResolve = null
+    });
 
-app.listen(port, () => {
-    console.log(`app is running at http://127.0.0.1:${port}/`)
-})
+});
 
+
+io.on('disconnect', () => {
+    console.log('disconnect',);
+});
+
+function getClientToken() {
+    io.emit('askToken');
+    return new Promise((resolve, reject) => {
+        askTaskResolve = resolve
+    });
+}
+
+
+server.listen(3000);
+console.log('Server running at http://localhost:3000/');
